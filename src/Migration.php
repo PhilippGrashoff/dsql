9f91e0302339128754ac6f58f20221e2a6893f30
Romans Malinovskis <romans@agiletoolkit.org>
2016-03-30 21:58:42 +0100

    work in progress

---
+++ src/Migration.php
@@ -0,0 +1,153 @@
+
+<?php // vim:ts=4:sw=4:et:fdm=marker
+
+namespace atk4\dsql;
+
+/**
+ * Perform a schema alteration query operation on SQL server (such as select, insert, delete, etc)
+ */
+class Query extends Expression
+{
+
+    /**
+     * addColumn() default type if 2nd argument is ommitted
+     *
+     * @var string|Expression
+     */
+    public $defaultColumnType = 'string';
+
+
+
+    // {{{ Field specification and rendering
+    /**
+     * Alters table by adding a new column
+     *
+     * @param string|array            $field Create one or several fields in table
+     * @param string|array|expression $type  Define type for this field(s)
+     *
+     * @return $this
+     */
+    public function addColumn($name, $type = null)
+    {
+
+        // Allow to specify comma-separated fields
+        if (is_string($name)) {
+            $name = array_map('trim',explode(',', $name));
+        }
+
+        if ($type === null) {
+            $type = $this->defaultColumnType;
+        }
+
+        if (is_string($type) || $type instanceof Expresson) {
+            $type = [$type];
+        } elseif (is_array($type)) {
+            if (!isset($type[0])) {
+
+                if (is_array($this->defaultColumnType)) {
+                    $type = array_merge($this->defaultColumnType, $type);
+                }else{
+                    $type[0] = $this->defaultColumnType;
+                }
+            }
+
+        } else {
+            throw new Exception(['Incorrect type declaration', 'type'=>$type]);
+        }
+
+        foreach($name as $n){
+            $this->args['columns'][$n] = $type;
+        }
+
+        return $this;
+    }
+
+    public function changeColumn($name, $type = null)
+    {
+        if (is_string($name)) {
+            $name = array_map('trim',explode(',', $name));
+        }
+
+        $this->addColumn($name,$type);
+
+        foreach($name as $n){
+            $this->args['columns'][$n]['change'] = true;
+        }
+
+        return $this;
+    }
+
+    public function dropColumn($name)
+    {
+        if (is_string($name) && strpos(',',$name) !== false) {
+            $name = explode(',', $name);
+        }
+
+        if (!is_array($this->args['droppings'])) {
+            $this->args['droppings'] = $name;
+        } else {
+            $this->args['droppings'] = array_merge(
+                $this->args['droppings'], $name
+            );
+        }
+
+        return $this;
+    }
+
+    function drop($option = null)
+    {
+        $this->template = $this->templates['drop'];
+        $this['drop_options'] = $option?:"";
+    }
+
+    function create()
+    {
+    }
+
+
+    /**
+     * TO BE REFACTORED
+     *
+     * @return string Parsed template chunk
+     */
+    protected function _render_table_noalias()
+    {
+        // will be joined for output
+        $ret = [];
+
+        if ($this->args['table'] instanceof Expression) {
+            throw new Exception('Table cannot be expression for UPDATE / INSERT queries in '.__METHOD__);
+        }
+
+        foreach ($this->args['table'] as $row) {
+            list($table, ) = $row;
+
+            $table = $this->_escape($table);
+
+            $ret[] = $table;
+        }
+
+        return implode(', ', $ret);
+    }
+
+    /// }}}
+
+    /**
+     * Switch template for this query. Determines what would be done
+     * on execute.
+     *
+     * By default it is in SELECT mode
+     *
+     * @param string $mode A key for $this->templates
+     *
+     * @return $this
+     */
+    public function selectTemplate($mode)
+    {
+        $this->mode = $mode;
+        $this->template = $this->templates[$mode];
+
+        return $this;
+    }
+
+}
---
+++ tests/db/MigrationTest.php
@@ -0,0 +1,84 @@
+<?php
+namespace atk4\dsql\tests;
+use atk4\dsql\Query;
+use atk4\dsql\Expression;
+use atk4\dsql\Migration;
+
+
+
+class MigrationTest extends \PHPUnit_Extensions_Database_TestCase
+{
+    protected $pdo;
+    function __construct()
+    {
+        $this->pdo = new \PDO( $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'] );
+        $this->pdo->query('create temporary table employee (id int, name text, surname text, retired bool)');
+    }
+    protected function getConnection()
+    {
+        return $this->createDefaultDBConnection($this->pdo, $GLOBALS['DB_DBNAME']);
+    }
+    protected function getDataSet()
+    {
+        return $this->createFlatXMLDataSet(dirname(__FILE__).'/SelectTest.xml');
+    }
+    private function m(){
+        return new Migration(['connection'=>$this->pdo]);
+    }
+    private function q(){
+        return new Query(['connection'=>$this->pdo]);
+    }
+    private function e($template=null,$args=null){
+        return new Expression(['connection'=>$this->pdo]);
+    }
+    public function testBasicMigrations()
+    {
+        // drop old table first
+        $this->m()->table('user')
+            ->drop('if exists');
+
+        $this->m()->table('user')
+            ->addColumn('name',['size'=>50])
+            ->addColumn('email,password')
+            ->addColumn('string','string')
+            ->addColumn('text','text') // string-based
+            ->addColumn('integer','integer')
+            ->addColumn('float','float')
+            ->addColumn('money','money') // float-based
+            ->addColumn('boolean','boolean')
+            ->addColumn('date','date')
+            ->addColumn('time','time')
+            ->addColumn('datetime','datetime')
+            ->addColumn('timestamp','timestamp')
+
+            ->create();
+
+        $this->m()->table('user')
+            ->changeColumn('name')
+            ->addColumn('surname')
+            ->addColumn('surname')
+            ->execute();
+
+        $this->m()->table('user')
+            ->dropColumn('string,text,integer,float,money,boolean,date,time,datetime,timestamp')
+            ->execute();
+
+        $this->q()->table('user')
+            ->set('name','Darcy')
+            ->set('surname','Wild')
+            ->insert();
+
+        $this->q()->table('user')
+            ->set(['name'=>'Brett','surname'=>'Bird'])
+            ->insert();
+
+        $this->q()->table('user')
+            ->insert([
+                ['name'=>'Jason','surname'=>'Wild'],
+                ['name'=>'Juliet','surname'=>'Wild'],
+                ['name'=>'James','surname'=>'Knight'],
+            ])
+            ->insert();
+    }
+}
+
